21bce1633f2b72b1aae9175d6eaf1bd11eecf02d
devlovas <devlovas@gmail.com>
2020-08-14 19:56:03 +0800

    Define project path

--- SipasteConst.py
+++ SipasteConst.py
@@ -5,5 +5,7 @@ import os
 
 CONFIG_FILE_NAME = 'Sipaste.sublime-settings'
 
+ENV_PATH = os.path.split(os.path.realpath(__file__))[0]
+
 SET_FILE_SOURCE = os.path.join(ENV_PATH, CONFIG_FILE_NAME)
 SET_FILE_BACKUP = os.path.join(ENV_PATH, CONFIG_FILE_NAME + '-backup')
