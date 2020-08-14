b0a4875c2351d5c1d40fdfa2456dc6dd0de642ce
devlovas <devlovas@gmail.com>
2020-08-14 19:43:03 +0800

    Define sublime-settings source/backup paths

---
+++ SipasteConst.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+
+CONFIG_FILE_NAME = 'Sipaste.sublime-settings'
+
+SET_FILE_SOURCE = os.path.join(ENV_PATH, CONFIG_FILE_NAME)
+SET_FILE_BACKUP = os.path.join(ENV_PATH, CONFIG_FILE_NAME + '-backup')
