eaf260e439399fdd091d9f97fdb0d5e9908f1987
devlovas <devlovas@gmail.com>
2020-08-14 20:02:29 +0800

    Add copy_file function

---
+++ SipastePublicUtil.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+
+def copy_file(source_path, target_path, new_name=None, binary=True):
+
+  """
+  拷贝文件：
+
+  :param source_path: <string>  : 原始文件路径
+  :param target_path: <string>  : 目标文件路径
+  :param [binary (:True)]: <boolean>  : 文件是否以二进制方式打开
+  :param [new_name (:None)]: <string> : 指定新的名称，默认为源文件名
+  :return: <boolean> file copy result
+  """
+
+  try:
+    if not os.path.exists(target_path): os.makedirs(target_path)                                         # 目标目录不存在则创建
+    target_path = os.path.join(target_path, new_name if new_name else os.path.split(source_path)[1])     # 包含文件名的目标目录
+
+    with open(source_path, 'rb' if binary else 'r') as fs:
+      with open(target_path, 'wb' if binary else 'w') as ft: ft.write(fs.read())                         # 将文件移动到目标目录
+
+    return True
+
+  except: return False
