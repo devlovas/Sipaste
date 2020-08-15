5719624b356c25601640665bbe02b73c6d836676
devlovas <devlovas@gmail.com>
2020-08-15 14:31:33 +0800

    Override the get image Name/Git image Path method

--- Sipaste.py
+++ Sipaste.py
@@ -16,12 +16,19 @@ not ENV_PATH in sys.path and sys.path.append(ENV_PATH)
 # Import external resources                                #
 ############################################################
 
+from SipasteImageUtil import get_image_path
+from SipasteImageUtil import get_image_name
+
 from SipastePublicUtil import initialize
 
 class SipasteCommand(sublime_plugin.TextCommand):
 
+  def get_image_name(self): return get_image_name(self)
+
+  def get_image_path(self): return get_image_path(self)
+
   @initialize
   def run(self, edit):
 
-    import SipasteImageUtil as image
-    print(image.get_image_path(self))
+    print(self.image_name)
+    print(self.image_path)
