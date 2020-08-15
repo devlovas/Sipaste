f2192f875a23dc1e69d09354b7074b5ebdc7c38e
devlovas <devlovas@gmail.com>
2020-08-15 18:56:05 +0800

    Import image manipulation tool

--- SipasteConst.py
+++ SipasteConst.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import os
+import ctypes
 
 CONFIG_FILE_NAME = 'Sipaste.sublime-settings'
 
@@ -9,5 +10,7 @@ ENV_PATH = os.path.split(os.path.realpath(__file__))[0]
 
 SET_FILE_SOURCE = os.path.join(ENV_PATH, CONFIG_FILE_NAME)
 SET_FILE_BACKUP = os.path.join(ENV_PATH, CONFIG_FILE_NAME + '-backup')
+
+IMAGETOOLS = ctypes.CDLL(os.path.join(ENV_PATH,'lib','IMAGE_TOOLS.dll'))
 
 CONFIG_LIST_ARGS = ['imageSavePath', 'imageTrashPath', 'imageNameInfo', 'imageExts']
